63e4b1abdebbfdee5529137a46419bcd6c1a03fa
Thanh <ongvuabom13@gmail.com>
2022-04-06 19:09:44 +0700

    upload lan 2

--- 61134360_TH6-4/src/thanh61134360/LuongLamViec.java
+++ 61134360_TH6-4/src/thanh61134360/LuongLamViec.java
@@ -51,6 +51,24 @@ public class LuongLamViec extends Thread{
 				
 				if(chuoiNhan.equals("1")) {
 					guifile("C:\\1.txt");
+				}else if(chuoiNhan.equals("2")) {
+					guifile("C:\\2.txt");
+				}else if(chuoiNhan.equals("3")) {
+					guifile("C:\\3.txt");
+				}else if(chuoiNhan.equals("4")) {
+					guifile("C:\\4.txt");
+				}else if(chuoiNhan.equals("5")) {
+					guifile("C:\\5.txt");
+				}else if(chuoiNhan.equals("6")) {
+					guifile("C:\\6.txt");
+				}else if(chuoiNhan.equals("7")) {
+					guifile("C:\\7.txt");
+				}else if(chuoiNhan.equals("8")) {
+					guifile("C:\\8.txt");
+				}else if(chuoiNhan.equals("9")) {
+					guifile("C:\\9.txt");
+				}else if(chuoiNhan.equals("10")) {
+					guifile("C:\\10.txt");
 				}
 			}
 			
