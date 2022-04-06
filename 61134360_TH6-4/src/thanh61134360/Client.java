a6724c3b873afebb33e24f7772d1e980cb7a4ef5
Thanh <ongvuabom13@gmail.com>
2022-04-06 19:04:51 +0700

    update

---
+++ 61134360_TH6-4/src/thanh61134360/Client.java
@@ -0,0 +1,44 @@
+package thanh61134360;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) {
+		try {
+			
+			Socket socket = new Socket("localhost",1111);
+			System.out.print("Conected!");
+			InputStream in = socket.getInputStream();
+			InputStreamReader inReader = new InputStreamReader(in);
+			BufferedReader buffR = new BufferedReader(inReader);
+
+			OutputStream osToClient = socket.getOutputStream();	
+			OutputStreamWriter write2Client = new OutputStreamWriter(osToClient);
+			BufferedWriter buffW = new BufferedWriter(write2Client);
+
+			Scanner banPhim = new Scanner(System.in);
+			while(true) {
+				System.out.print("\nClient:");
+				String chuoiGui = banPhim.nextLine();
+				buffW.write(chuoiGui+"\n");
+				buffW.flush();
+				String chuoiNhan = buffR.readLine();
+				System.out.print("Server: "+ chuoiNhan);
+				if(chuoiGui.equals("10")) break;
+			}
+			socket.close();
+			
+		}catch(Exception e) {
+			System.out.print(e.getMessage());
+		}
+	}
+
+}
---
+++ 61134360_TH6-4/src/thanh61134360/LuongLamViec.java
@@ -0,0 +1,61 @@
+package thanh61134360;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class LuongLamViec extends Thread{
+	Socket socketClient;
+	int id = -1;
+	public LuongLamViec(Socket socketClient, int id) {
+		super();
+		this.socketClient = socketClient;
+		this.id = id;
+	}
+	void guifile(String tenFile)throws IOException{
+		FileReader fr = new FileReader(tenFile);
+		BufferedReader buffR = new BufferedReader(fr);
+		OutputStream osToClient = socketClient.getOutputStream();
+		OutputStreamWriter write2client = new OutputStreamWriter(osToClient);
+		BufferedWriter buffW = new BufferedWriter(write2client);
+		String line;
+		while((line=buffR.readLine()) != null) {
+			buffW.write(line);
+			buffW.flush();
+		}
+		fr.close();
+	}
+	@Override
+	public void run() {
+		try {
+			System.out.print(socketClient.getInetAddress().getHostAddress());
+			System.out.print(id);
+			
+			OutputStream osToClient = socketClient.getOutputStream();
+			OutputStreamWriter write2client = new OutputStreamWriter(osToClient);
+			BufferedWriter buffW = new BufferedWriter(write2client);
+			
+			InputStream in = socketClient.getInputStream();
+			InputStreamReader inReader = new InputStreamReader(in);
+			BufferedReader buffR = new BufferedReader(inReader);
+			
+			while(true) {
+				String chuoiNhan=buffR.readLine();
+				System.out.print("\n"+chuoiNhan);
+				
+				if(chuoiNhan.equals("1")) {
+					guifile("C:\\1.txt");
+				}
+			}
+			
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+	}
+}
